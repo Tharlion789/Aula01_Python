c6ca67812cbcb24dd235d4d7232ebc692af2ea34
Tharlion789 <140929300+Tharlion789@users.noreply.github.com>
2024-10-02 20:42:01 -0300

    Exercicio 03
    1- Soma de dois numeros e mostrar resultado
    2- Concatenar string: insira o primeiro nome e o sobrenome do usuario em duas variaveis, e exiba o nome completo
    3- Calcule a media de tres notas e no final mostre a media
    4- area de um triangulo

---
+++ ex03.py
@@ -0,0 +1,27 @@
+#calculo
+n1 = 500
+n2 = 250
+n3 = n1 + n2
+
+print("A soma dos valores:", n1, "e", n2, "é", n3)
+
+#nome + sobrenome
+nome = ("tharlion")
+sobrenome = ("rodrigues")
+
+print("nome completo:", nome, sobrenome)
+#media nota
+nota1 = 60
+nota2 = 60
+nota3 = 55
+
+nota_final = nota1 + nota3 / 4
+
+print("Media final:", nota_final)
+
+#triangulo
+area = 50
+altura = 75
+triangulo = area * altura
+
+print("Area de um triangulo:", triangulo)
